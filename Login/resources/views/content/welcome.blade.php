079e89f063ccf47a5a1f616fefafa97159a12a87
Jairon Landa <intern.jairon@digiheritage.com.my>
2015-09-10 08:40:48 +0800

    Login, Register, and Resetpassord

---
+++ Login/resources/views/content/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+    
+    <h1> </h1>
+    
+@stop
---
+++ Login/resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    
+</head>
+<body class="bg-info">
+ 
+
+    
+
+    @yield('content')       
+     
+
+    
+</body> 
+</html>
